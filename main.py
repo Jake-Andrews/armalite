42b12028722c974cb9ad5e3ecff9c8aa2b443e7a
Jake-Andrews <jake.andrews1@ontariotechu.net>
2023-11-30 01:55:02 -0500

    naive hamming distance Ouputting to console only. code is very messy Update .gitignore to ignore image file ext

--- main.py
+++ main.py
@@ -3,6 +3,38 @@ import os
 from PIL import Image
 import io
 from pathlib import Path
+import cv2
+import itertools
+from collections import defaultdict
+#Take a list of images
+#Maintain two lists of images
+#1st list - Original images and a list of resized images used in hamming distance
+#Resize the original images every time  and keep aspect ratio every time the user clicks on a row, to display it.
+#Hamming distance algorithm:
+#Convert to black and white
+#Resize to 30x30
+#Flatten the image. Optionally flatten it both row wise and column wise. 
+#
+#image = cv2.imread(args.file)
+#image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+#grayscale_image = cv2.cvtColor(np_image, cv2.COLOR_RGB2GRAY)  
+#Compare two image strings. If they are idential hamming distance is 0. 
+#If they differ by 1 value hamming distance is 1, etc... 
+
+#Makes more sense to have an original table then a seperate table for each algorithm.
+
+def hamming_distance(image1, image2):
+    #Convert to strings
+
+    #image_length
+
+    #use itertools.combinations(dict, 2) to save computation. 
+    #a, b, c, d, e   ab ac ad ae  avoid-> ba bc de instead-> bc bd be aka use itertools.combinations
+
+    return
+
+def rgb_to_grey(image):
+    return cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
 def get_image_data(file, max_size=(400, 300)):
     """
@@ -14,7 +46,16 @@ def get_image_data(file, max_size=(400, 300)):
     img.save(bio, format="PNG")
     return bio.getvalue()
 
-def main():
+def get_cv2_image(image_path):
+    try:
+        image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        return image
+    except Exception as e:
+        print(f'Error loading image: {e}\nFilepath: {image_path}')
+        return 'rut roh' #Return a blank error message later
+
+def define_window_layout():
     # Define the layout for the window
     tree_layout = [
         [
@@ -55,12 +96,13 @@ def main():
         ],
         [
             sg.Text('Resize algorithm'),
-            sg.Combo(['Lanczos3', 'Bilinear', 'Bicubic'], default_value='Lanczos3', key='-RESIZE-'),
+            sg.Combo(['Hamming Distance No Hash', 'Bilinear', 'Bicubic'], default_value='Hamming Distance No Hash', key='-RESIZE-'),
             sg.Text('Hash size'),
             sg.Spin([i for i in range(1, 33)], initial_value=16, key='-HASHSIZE-'),
             sg.Text('Hash type'),
             sg.Combo(['Gradient', 'Blockhash'], default_value='Gradient', key='-HASHTYPE-'),
-            sg.Checkbox('Ignore same size', default=True, key='-IGNORESIZE-')
+            sg.Checkbox('Ignore same size', default=True, key='-IGNORESIZE-'),
+            sg.Button('Run Algorithm', expand_x=True),
         ],
         [
             sg.Text('Similarity'),
@@ -75,9 +117,39 @@ def main():
     ]
 
     # Create the Window with specified size
-    window = sg.Window('File Management System', layout, resizable=True)
+    return sg.Window('File Management System', layout, resizable=True)
 
-    # Event loop
+def resize(image, height=8, width=8):
+    flattened = cv2.resize(image,(height, width), interpolation = cv2.INTER_AREA).flatten()
+    print(f'Flattened: {flattened.shape}')
+    #flattened_column = cv2.resize(image,(height, width), interpolation = cv2.INTER_AREA).flatten('F')
+    return flattened
+
+def hamming_distance(image, image1):
+    #count = 0
+    #for index, pixel_value in enumerate(image): 
+    #    if pixel_value != image1[index]:
+    #        count += 1
+    print(f'Length: {len(image)}')
+    print(f'Length1: {len(image1)}')
+    count = sum(1 for a, b in zip(image, image1) if a != b)
+    return count
+#Change image_list to a dictionary with the original filenames
+#So when they are returned it will be easy to show what pictures are
+def hamming_distance_naive(image_list, threshold=10):
+    dict_duplicates = defaultdict(list)
+    for k1, k2, in itertools.combinations(image_list, 2):
+        print(f'{k1}, {k2}, value: {hamming_distance(image_list[k1], image_list[k2])}')
+        if hamming_distance(image_list[k1], image_list[k2]) < threshold:
+            dict_duplicates[k1] = k2
+
+    return dict_duplicates
+
+def main():
+    window = define_window_layout()
+    file_names = []
+    image_file_names = {}
+
     while True:
         event, values = window.read()
         if event == sg.WIN_CLOSED:
@@ -85,38 +157,52 @@ def main():
         
         if event == '-TREE-':  # Event triggered when an item in the tree is clicked
             try:
-                # Assuming the tree data includes file paths, update the image preview
-                selected_file = window.Element('-TREE-').SelectedRows[0]  # Get the file path of the selected item
+                selected_file = window.Element('-TREE-').SelectedRows[0]  # Get the index of the selected row
                 #print(f'1: {selected_file}')
                 #print(f'2: {window.Element("-TREE-").TreeData}')
                 #print(f'3: {window.Element("-TREE-").TreeData.tree_dict[selected_file].values[1]}')
-                print(f'4: {window.Element("-TREE-").TreeData.tree_dict[selected_file].text}')
+                print(f'4: {window.Element("-TREE-").TreeData.tree_dict[selected_file].text}') 
                 #print(window.Element('-TREE-').TreeData.tree_dict[selected_file])
-                full_filename_path = window.Element("-TREE-").TreeData.tree_dict[selected_file].text
+                full_filename_path = window.Element("-TREE-").TreeData.tree_dict[selected_file].text # text property of row index
                 path = Path(full_filename_path)
                 image_data = get_image_data(path)
                 window['-IMAGE-'].update(image_data)
             except Exception as e:
                 sg.popup_error('Error loading image:', e)
+        elif event == 'Run Algorithm':
+            algorithm = values['-RESIZE-']
+            if image_file_names:
+                if algorithm == 'Hamming Distance No Hash':
+                    black_white_resized_images = {}
+                    for key, value in image_file_names.items():
+                        temp_image = (rgb_to_grey(value))
+                        black_white_resized_images[key] = (resize(temp_image))
+                    dict_list = hamming_distance_naive(black_white_resized_images)
 
+                    print(dict_list)
+
+                    print('sneed')
+            else: 
+                print('Error running algorithm, file list must not be empty!')
         elif event == 'Search':
             folder = values['-FOLDER-']
             try:
-                # Get list of files in folder
                 file_list = os.listdir(folder)
             except:
                 file_list = []
-
+            # Get list of files and create dictionary 
             file_names = [
                 os.path.join(folder, f) for f in file_list
                 if os.path.isfile(os.path.join(folder, f)) and f.lower().endswith(('.png', '.gif', '.jpg', '.jpeg', '.tiff', '.bmp'))
             ]
+            for file_name in file_names: 
+                image_file_names[file_name] = get_cv2_image(file_name)
 
-                        # Prepare data for the tree
+            # Prepare data for the tree
             tree_data = sg.TreeData()
-            for index, filename in enumerate(file_names):
-                #image_data = get_image_data(filename)
-                tree_data.insert('', index, filename, values=['', filename])
+            for index, (key, value) in enumerate(image_file_names.items()):
+                #print(index, key, value)
+                tree_data.insert('', index, key, values=[f'{value.shape[:1]} x {value.shape[1:2]}', key])
 
             # Update the tree with the images
             window['-TREE-'].update(values=tree_data)
